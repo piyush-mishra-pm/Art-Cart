68a7fd12858834b8c534eedc6ea5a8c24159242f
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-01-27 15:09:44 +0530

    Re: Added first mock route and controller.

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ app.use(express.json());
 // Importing All Routes here:
 const products = require('./routes/product');
 
+// Binding routes here:
 app.use('/api/v1', products);
 
 module.exports=app;

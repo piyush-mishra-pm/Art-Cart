4a382f499c096e12468b76f138ecd39d4a1f6fb5
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-01-28 13:07:58 +0530

    Errors now send different responses for development mode and production mode.

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -2,9 +2,20 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (error,req,res,next)=>{
     error.statusCode = error.statusCode || 500; // Default error code is 500: internal server error.
-    error.message = error.message || 'Internal Server Error';
-    res.status(error.statusCode).json({
-        success: false,
-        error: error.stack
-    });
+    
+    if(process.env.NODE_ENV === 'DEVELOPMENT'){
+        res.status(error.statusCode).json({
+            success: false,
+            message: error.message || 'Internal Server Error',
+            error: error,
+            stack: error.stack
+        });
+    }
+
+    if(process.env.NODE_ENV === 'PRODUCTION'){
+        res.status(error.statusCode).json({
+            success: false,
+            message: error.message || 'Internal Server Error'
+        });
+    }
 }

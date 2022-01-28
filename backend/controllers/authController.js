8ec90345aa23f7e4c135ab0d28a265f4313951db
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-01-28 20:23:36 +0530

    User Login and assigning JWT token.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -24,3 +24,34 @@ exports.registerUser = catchAsyncErrors(async(req,res,next) => {
         token
     });
 });
+
+// Login User via => /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // check if email and password are valid:
+    if (!email || !password) {
+        return next(new ErrorHandler('Please enter email and password', 400));
+    }
+
+    // Finding User in DB:
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return next(new ErrorHandler('Invalid Email or Password', 401)); // 401 is unAuthenticated user.
+    }
+
+    // Verifying Password:
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched)
+        return next(new ErrorHandler('Invalid Email or Password', 401)); // 401 is unAuthenticated user.
+    
+    // User name and password correct, so assign Token:
+    const jwtToken = user.getJwtToken();
+
+    res.status(200).json({
+        success: true,
+        jwtToken
+    })
+})

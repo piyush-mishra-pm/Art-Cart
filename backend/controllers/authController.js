725c2817e6c91dd16b24a9a5cd628c1102e314ff
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-01-28 20:47:50 +0530

    Cookies and JWT generated on login and user registeration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 // Register a User via => /api/v1/register
 exports.registerUser = catchAsyncErrors(async(req,res,next) => {
@@ -17,12 +18,7 @@ exports.registerUser = catchAsyncErrors(async(req,res,next) => {
         }
     );
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    });
+    sendToken(user,200,res);
 });
 
 // Login User via => /api/v1/login
@@ -48,10 +44,5 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
         return next(new ErrorHandler('Invalid Email or Password', 401)); // 401 is unAuthenticated user.
     
     // User name and password correct, so assign Token:
-    const jwtToken = user.getJwtToken();
-
-    res.status(200).json({
-        success: true,
-        jwtToken
-    })
+    sendToken(user, 200, res);
 })

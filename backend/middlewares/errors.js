70e285e61af4edf5bbc5d161c8c8b1f9e58db83b
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-01-28 15:05:09 +0530

    Mongoose CastError and ValidationErrors handled now.

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -2,7 +2,7 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (error,req,res,next)=>{
     error.statusCode = error.statusCode || 500; // Default error code is 500: internal server error.
-    
+
     if(process.env.NODE_ENV === 'DEVELOPMENT'){
         res.status(error.statusCode).json({
             success: false,
@@ -13,9 +13,23 @@ module.exports = (error,req,res,next)=>{
     }
 
     if(process.env.NODE_ENV === 'PRODUCTION'){
-        res.status(error.statusCode).json({
+        let errorCopy = {...error};
+        errorCopy.message = error.message || 'Internal Server Error';
+
+        // Wrong Mongoose ObjectID Error:
+        if(error.name === 'CastError'){
+            const message = `Resource not found. Invalid: ${error.path}`;
+            errorCopy = new ErrorHandler(message,400);
+        }
+        // Handling Mongoose Validation Errors:
+        else if(error.name === 'ValidationError'){
+            const message = Object.values(error.errors).map(value=> value.message);
+            errorCopy = new ErrorHandler(message, 400);
+        }
+
+        res.status(errorCopy.statusCode).json({
             success: false,
-            message: error.message || 'Internal Server Error'
+            message: errorCopy.message,
         });
     }
 }

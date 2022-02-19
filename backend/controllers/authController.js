e9eed2f31f5b8ff00418ae287fd0212ffffbdb52
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-02-19 16:17:42 +0530

    Admin can update and delete a user.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -213,3 +213,42 @@ exports.getUserDetails = catchAsyncErrors(async (req,res,next)=>{
         user
     })
 })
+
+// Update user profile => /api/v1/admin/user/:id:
+exports.updateUser = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name:req.body.name,
+        email:req.body.email,
+        role:req.body.role
+    }
+
+    // Update User data:
+    const user = await User.findByIdAndUpdate(req.params.id,newUserData,{
+        new:true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+    })
+})
+
+// Delete User => /api/v1/admin/user/:id
+exports.deleteUser = catchAsyncErrors(async (req,res,next)=>{
+
+    const user = await User.findById(req.params.id);
+
+    if(!user){
+        return next(new ErrorHandler(`No user found with id: ${req.params.id} `));
+    }
+
+    // Remove avatar from CLoudinary: TODO.
+
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,35 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-const { 
-    registerUser, 
-    loginUser, 
-    logout, 
-    forgotPassword, 
-    resetPassword,
-    getUserProfile,
-    updatePassword,
-    updateProfile,
-    allUsers,
-    getUserDetails,
-} = require('../controllers/authController');
+const authController = require('../controllers/authController');
 
-const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth')
+const authMiddleware = require('../middlewares/auth')
 
-router.route('/register').post(registerUser);
-router.route('/login').post(loginUser);
-router.route('/logout').get(logout);
+router.route('/register').post(authController.registerUser);
+router.route('/login').post(authController.loginUser);
+router.route('/logout').get(authController.logout);
 
-router.route('/password/forgot').post(forgotPassword);
-router.route('/password/reset/:token').put(resetPassword);
+router.route('/password/forgot').post(authController.forgotPassword);
+router.route('/password/reset/:token').put(authController.resetPassword);
 
-router.route('/me').get(isAuthenticatedUser,getUserProfile);
-router.route('/password/update').put(isAuthenticatedUser, updatePassword);
-router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+router.route('/me').get(authMiddleware.isAuthenticatedUser, authController.getUserProfile);
+router.route('/password/update').put(authMiddleware.isAuthenticatedUser, authController.updatePassword);
+router.route('/me/update').put(authMiddleware.isAuthenticatedUser, authController.updateProfile);
 
 // ADMIN-Routes:
-router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
-router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails);
+router.route('/admin/users').get(authMiddleware.isAuthenticatedUser, authMiddleware.authorizeRoles('admin'), authController.allUsers);
+
+router.route('/admin/user/:id')
+    .get(authMiddleware.isAuthenticatedUser, authMiddleware.authorizeRoles('admin'), authController.getUserDetails)
+    .put(authMiddleware.isAuthenticatedUser, authMiddleware.authorizeRoles('admin'), authController.updateUser)
+    .delete(authMiddleware.isAuthenticatedUser, authMiddleware.authorizeRoles('admin'), authController.deleteUser);
 
 
 module.exports = router;
